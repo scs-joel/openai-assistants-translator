118ae77be061cc216c08bdd5c06e5e6e280e23aa
joel <joel@skeletoncrew.co.jp>
2025-03-12 23:01:43 +0900

    refactor: change folder structure

---
+++ src/app/api/assistants/threads/[threadId]/messages/route.ts
@@ -0,0 +1,20 @@
+import { assistantId } from "@/assistant-config";
+import { openai } from "@/openai";
+
+export const runtime = "nodejs";
+
+// Send a new message to a thread
+export async function POST(request, { params: { threadId } }) {
+  const { content } = await request.json();
+
+  await openai.beta.threads.messages.create(threadId, {
+    role: "user",
+    content: content,
+  });
+
+  const stream = openai.beta.threads.runs.stream(threadId, {
+    assistant_id: assistantId,
+  });
+
+  return new Response(stream.toReadableStream());
+}
