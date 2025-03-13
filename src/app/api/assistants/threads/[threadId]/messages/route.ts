9f0336599449ee17a508bebebd8a70973c6f558b
joel <joel@skeletoncrew.co.jp>
2025-03-13 12:32:06 +0900

    fix

--- src/app/api/assistants/threads/[threadId]/messages/route.ts
+++ src/app/api/assistants/threads/[threadId]/messages/route.ts
@@ -1,11 +1,17 @@
+import { NextRequest } from "next/server";
+
 import { assistantId } from "@/config/assistant-config";
 import { openai } from "@/config/openai";
 
 export const runtime = "nodejs";
 
 // Send a new message to a thread
-export async function POST(request, { params: { threadId } }) {
+export async function POST(
+  request: NextRequest,
+  { params }: { params: Promise<{ threadId: string }> },
+) {
   const { content } = await request.json();
+  const { threadId } = await params;
 
   await openai.beta.threads.messages.create(threadId, {
     role: "user",
--- src/app/api/files/[fileId]/route.ts
+++ src/app/api/files/[fileId]/route.ts
@@ -1,7 +1,14 @@
+import { NextRequest } from "next/server";
+
 import { openai } from "@/config/openai";
 
 // download file by file ID
-export async function GET(_request, { params: { fileId } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ fileId: string }> },
+) {
+  const { fileId } = await params;
+
   const [file, fileContent] = await Promise.all([
     openai.files.retrieve(fileId),
     openai.files.content(fileId),
